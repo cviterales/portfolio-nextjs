66a577f048c2cd24099812ba801f91ab8a52c157
Cristian Viterales <cviterales@gmail.com>
2022-10-25 22:59:03 -0300

    test deploy vercel

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Projects from "../components/Projects/Projects";
 import About from "../components/About/About";
 import Footer from "../components/Footer/Footer";
 
+
 export default function App() {
   return (
     <div className="wrapper">
